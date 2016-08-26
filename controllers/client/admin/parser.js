e05359e23b6f96a44e6274e942d273986b8701be
rexdiamante28 <rexdiamante28@gmail.com>
2016-08-26 09:59:40 +0800

    commit css change

--- controllers/client/admin/parser.js
+++ controllers/client/admin/parser.js
@@ -6,3 +6,15 @@ Template.textparser.helpers({
         return sessionStorage.getItem('selectedPublicationId');
     },
 })
+
+Template.textparser.events({
+    'submit form': function(event,template){
+        event.preventDefault();
+
+        var categoryId = template.find('#categoryId').value;
+        var publicationId= template.find('#publicationId').value;
+        var canvasContent= template.find('#canvas').value;
+
+        console.log(canvasContent);
+    }
+})
